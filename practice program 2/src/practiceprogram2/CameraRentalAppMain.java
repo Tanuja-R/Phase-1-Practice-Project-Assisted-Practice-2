ec897fb2a12a8d28436058f6b6d58125afb758e6
Tanuja R <tanujar.18ec@saividya.ac.in>
2023-12-07 11:46:24 +0530

    Phase project Camera rental application

---
+++ practice program 2/src/practiceprogram2/CameraRentalAppMain.java
@@ -0,0 +1,11 @@
+package com.phaseOne;
+
+public class CameraRentalAppMain {
+
+	public static void main(String[] args) { 
+
+
+    	CameraRentalApp app = new CameraRentalApp(); 
+    	app.login(); 
+	}
+}
